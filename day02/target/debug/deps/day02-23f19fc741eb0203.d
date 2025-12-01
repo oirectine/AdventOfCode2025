8810191c12294e552efb4e79db02d0a84b61c6ae
David <dpkuling@gmail.com>
2025-12-01 19:13:58 +0000

    modified .gitignore

--- day02/target/debug/deps/day02-23f19fc741eb0203.d
+++
@@ -1,5 +0,0 @@
-/home/david/AdventOfCode2025/day02/target/debug/deps/day02-23f19fc741eb0203.d: src/main.rs
-
-/home/david/AdventOfCode2025/day02/target/debug/deps/libday02-23f19fc741eb0203.rmeta: src/main.rs
-
-src/main.rs:
